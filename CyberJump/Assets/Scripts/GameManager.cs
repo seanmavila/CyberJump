bbb206f4c14d66548acd49189fa7cbd341806b88
Sean Avila <sean.avila94@gmail.com>
2022-02-08 12:18:27 -0600

    camera movement and background
    - camera moves when play reaches top or bottom of the screen
    
    - backgrounds added - need to blend

---
+++ CyberJump/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Camera mainCamera;
+    public GameObject playerRb;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        MoveCamera();
+    }
+
+    public void MoveCamera()
+    {
+        if (playerRb.transform.position.y >= (mainCamera.transform.position.y + mainCamera.orthographicSize + 1f))
+        {
+            mainCamera.transform.Translate(0f, mainCamera.orthographicSize * 2, 0f);
+        }
+
+        if (playerRb.transform.position.y < (mainCamera.transform.position.y - mainCamera.orthographicSize - 1f))
+        {
+            mainCamera.transform.Translate(0f, -mainCamera.orthographicSize * 2, 0f);
+        }      
+    }
+}
