40bc8b6a36e7ae8d187f15055957bd3abbf0bbaa
Sean Avila <sean.avila94@gmail.com>
2022-02-08 21:15:12 -0600

    Tilemap
    added tilemap
    began layout jump puzzle

--- CyberJump/Assets/Scripts/GameManager.cs
+++ CyberJump/Assets/Scripts/GameManager.cs
@@ -18,12 +18,12 @@ public class GameManager : MonoBehaviour
 
     public void MoveCamera()
     {
-        if (playerRb.transform.position.y >= (mainCamera.transform.position.y + mainCamera.orthographicSize + 1f))
+        if (playerRb.transform.position.y >= (mainCamera.transform.position.y + mainCamera.orthographicSize))
         {
             mainCamera.transform.Translate(0f, mainCamera.orthographicSize * 2, 0f);
         }
 
-        if (playerRb.transform.position.y < (mainCamera.transform.position.y - mainCamera.orthographicSize - 1f))
+        if (playerRb.transform.position.y < (mainCamera.transform.position.y - mainCamera.orthographicSize))
         {
             mainCamera.transform.Translate(0f, -mainCamera.orthographicSize * 2, 0f);
         }      
